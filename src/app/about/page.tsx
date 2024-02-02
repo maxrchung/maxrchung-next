96f1fef530beb006d378801b6e0d5cf97f628495
maxrchung <maxrchung@gmail.com>
2024-02-02 05:51:03 -0800

    start some mdx shenanigans

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,94 +1,9 @@
 "use client";
 
-import {
-  Avatar,
-  MenuIcon,
-  Pane,
-  Paragraph,
-  Popover,
-  Position,
-  majorScale,
-} from "evergreen-ui";
-import NavLink from "../../components/Nav/NavLink";
-import Link from "next/link";
-import NavIcon from "../../components/Nav/NavIcon";
+// We have to import Content like this because we need to specify "use client" for Evergreen
+import Content from "./content.mdx";
+import "./style.module.css";
 
-export default function About() {
-  return (
-    <>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </Paragraph>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-    </>
-  );
+export default function Page() {
+  return <Content />;
 }
---
+++ src/mdx-components.tsx
@@ -0,0 +1,51 @@
+import {
+  Code,
+  CodeProps,
+  Heading,
+  HeadingProps,
+  Link,
+  LinkProps,
+  ListItem,
+  ListItemProps,
+  OrderedList,
+  OrderedListProps,
+  Paragraph,
+  ParagraphProps,
+  Pre,
+  PreProps,
+  Small,
+  SmallProps,
+  Strong,
+  StrongProps,
+  UnorderedList,
+  UnorderedListProps,
+} from "evergreen-ui";
+import type { MDXComponents } from "mdx/types";
+
+// This file allows you to provide custom React components
+// to be used in MDX files. You can import and use any
+// React component you want, including inline styles,
+// components from other libraries, and more.
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    // Allows customizing built-in components, e.g. to add styling.
+    // Following Evergreen Typography: https://evergreen.segment.com/foundations/typography#pre_component
+    h1: (props) => <Heading is="h1" size={900} {...(props as HeadingProps)} />,
+    h2: (props) => <Heading is="h2" size={800} {...(props as HeadingProps)} />,
+    h3: (props) => <Heading is="h3" size={700} {...(props as HeadingProps)} />,
+    h4: (props) => <Heading is="h4" size={600} {...(props as HeadingProps)} />,
+    h5: (props) => <Heading is="h5" size={500} {...(props as HeadingProps)} />,
+    h6: (props) => <Heading is="h6" size={400} {...(props as HeadingProps)} />,
+    p: (props) => <Paragraph {...(props as ParagraphProps)} />,
+    a: (props) => <Link {...(props as LinkProps)} />,
+    strong: (props) => <Strong {...(props as StrongProps)} />,
+    small: (props) => <Small {...(props as SmallProps)} />,
+    pre: (props) => <Pre {...(props as PreProps)} />,
+    code: (props) => <Code {...(props as CodeProps)} />,
+    ol: (props) => <OrderedList {...(props as OrderedListProps)} />,
+    ul: (props) => <UnorderedList {...(props as UnorderedListProps)} />,
+    li: (props) => <ListItem {...(props as ListItemProps)} />,
+    ...components,
+  };
+}
