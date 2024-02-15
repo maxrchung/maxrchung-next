1a45167147be0e6e441bb8bebe3b41794023f543
maxrchung <maxrchung@gmail.com>
2024-02-15 11:51:27 -0800

    prevent propagation on texxt click

--- src/components/Main/Gallery/Viewer.tsx
+++ src/components/Main/Gallery/Viewer.tsx
@@ -100,6 +100,7 @@ export default function Viewer({
             className={classNames({
               [style.loaded]: state === ViewerState.Loaded,
             })}
+            onClick={(event) => event.stopPropagation()}
           >
             {galleryConfigs[index].caption}
           </p>
