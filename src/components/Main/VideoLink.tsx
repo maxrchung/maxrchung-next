8b77fc607620ce8540ad9a2260f4373142fe567e
maxrchung <maxrchung@gmail.com>
2024-02-27 07:12:58 -0800

    oh yeah finished this maxrchungnext post

--- src/components/Main/VideoLink.tsx
+++ src/components/Main/VideoLink.tsx
@@ -1,13 +1,13 @@
-import { Image, Link as EvergreenLink, majorScale } from "evergreen-ui";
 import Link from "next/link";
+import { Link as EvergreenLink, majorScale } from "evergreen-ui";
 
-interface ImageLinkProps {
+interface VideoLinkProps {
   href: string;
   src: string;
   width: number;
 }
 
-export default function ImageLink({ href, src, width }: ImageLinkProps) {
+export default function VideoLink({ href, src, width }: VideoLinkProps) {
   return (
     <EvergreenLink
       is={Link}
@@ -18,13 +18,17 @@ export default function ImageLink({ href, src, width }: ImageLinkProps) {
       alignSelf="center"
       maxWidth="80%"
     >
-      <Image
+      <video
         src={src}
         width={width}
         // Counteract main styling, this looks pretty bad >_<
         style={{
           maxWidth: "100%",
         }}
+        autoPlay
+        playsInline
+        muted
+        loop
       />
     </EvergreenLink>
   );
